bc9faf54c74816fc5a14ec5e431ca86a38293c1a
Alireza abdolmaleki <alirezaabdolmaleki9080.com>
2024-07-10 23:36:01 +0330

    Create award_user table, modify user table and Database Seeder

--- database/migrations/2024_07_10_232024_create_award_user_table.php
+++ database/migrations/2024_07_10_232024_create_award_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAwardUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('award_user', function (Blueprint $table) {
             $table->id();
-            $table->integer('points')->default(100);
-            $table->timestamps();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('award_id')->constrained('awards');
+            $table->timestamp('time')->useCurrent();
         });
     }
 
@@ -27,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('award_users');
     }
 }
